ec6eda435b2a382748f6fd2fd2acdfec7d5c48ab
cqs263 <cqs263@hotmail.com>
2024-04-30 11:24:35 +0800

    升级到flask 3.0.1

--- app_configs.py
+++ app_configs.py
@@ -1,5 +1,12 @@
+import os
+
 from eb_utils.xs_json import XsJson
 
-CF = XsJson("conf/setting.json")
-CF_APP = CF.load()
 
+cf_path = 'conf/setting.json'
+# 检查文件是否存在
+if os.path.exists(cf_path):
+    CF = XsJson("conf/setting.json")
+    CF_APP = CF.load()
+else:
+    print(f"在vercel上运行忽略setting.json请在环境变量设置配置。")
